ee48fd417f12c6edb28a7a516ee00586c3ffc89c
evgeniy <e.a.rashchepkin@gmail.com>
2021-07-31 18:16:19 +0500

    Remove unwanted changes

--- datanymizer_engine/src/transformers/internet/email.rs
+++ datanymizer_engine/src/transformers/internet/email.rs
@@ -1,7 +1,4 @@
-use crate::{
-    transformer::{TransformContext, UniqTransformer, Uniqueness},
-    utils,
-};
+use crate::transformer::{TransformContext, UniqTransformer, Uniqueness};
 use fake::faker::internet::raw::*;
 use fake::locales::EN;
 use fake::Fake;
@@ -18,13 +15,11 @@ use serde::{Deserialize, Serialize};
 ///     email:
 ///       kind: Safe
 /// ```
-#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Clone, Debug, Default)]
+#[derive(Serialize, Deserialize, PartialEq, Eq, Hash, Clone, Debug)]
 #[serde(default)]
 pub struct EmailTransformer {
     /// Email kind (`Safe`, `Free`; `Safe` is default)
     pub kind: EmailKind,
-    pub prefix: usize,
-    pub suffix: usize,
     pub uniq: Uniqueness,
 }
 
@@ -37,22 +32,18 @@ pub enum EmailKind {
     Safe,
 }
 
-impl Default for EmailKind {
-    fn default() -> Self {
-        Self::Safe
-    }
-}
-
-const CHARS: &[char] = &[
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-];
-
-const AFFIX_SEPARATOR: char = '-';
-
 impl EmailTransformer {
     pub fn new() -> Self {
         Self::default()
+    }
+}
+
+impl Default for EmailTransformer {
+    fn default() -> Self {
+        Self {
+            kind: EmailKind::Safe,
+            uniq: Uniqueness::default(),
+        }
     }
 }
 
@@ -63,31 +54,9 @@ impl UniqTransformer for EmailTransformer {
         _field_value: &str,
         _ctx: &Option<TransformContext>,
     ) -> String {
-        let mut email: String = match self.kind {
+        match self.kind {
             EmailKind::Free => FreeEmail(EN).fake(),
             EmailKind::Safe => SafeEmail(EN).fake(),
-        };
-
-        if self.suffix > 0 {
-            let parts: Vec<&str> = email.splitn(2, '@').collect();
-            email = format!(
-                "{}{}{}@{}",
-                parts[0],
-                AFFIX_SEPARATOR,
-                utils::rnd_chars(self.suffix, CHARS),
-                parts[1]
-            );
-        };
-
-        if self.prefix > 0 {
-            format!(
-                "{}{}{}",
-                utils::rnd_chars(self.prefix, CHARS),
-                AFFIX_SEPARATOR,
-                email
-            )
-        } else {
-            email
         }
     }
 
@@ -123,59 +92,13 @@ mod tests {
         }
     }
 
-    mod transform {
-        use super::*;
+    #[test]
+    fn transform() {
+        let transformer = EmailTransformer::new();
+        let result = transformer.transform("field", "", &None).unwrap().unwrap();
+        let user_and_domain: Vec<_> = result.split('@').collect();
 
-        fn user_and_domain(config: &str) -> Vec<String> {
-            let transformer: EmailTransformer = serde_yaml::from_str(config).unwrap();
-            let result = transformer.transform("field", "", &None).unwrap().unwrap();
-            result.split('@').map(String::from).collect()
-        }
-
-        #[test]
-        fn default() {
-            let user_and_domain = user_and_domain("{}");
-
-            assert_eq!(user_and_domain.len(), 2);
-            assert!(user_and_domain[1].starts_with("example."));
-        }
-
-        #[test]
-        fn prefix() {
-            let user_and_domain = user_and_domain("prefix: 5");
-
-            assert_eq!(user_and_domain.len(), 2);
-            assert_eq!(user_and_domain[0].chars().nth(5).unwrap(), AFFIX_SEPARATOR);
-            assert!(user_and_domain[0].len() > 7);
-        }
-
-        #[test]
-        fn suffix() {
-            let user_and_domain = user_and_domain("suffix: 4");
-
-            assert_eq!(user_and_domain.len(), 2);
-            assert_eq!(
-                user_and_domain[0].chars().nth_back(4).unwrap(),
-                AFFIX_SEPARATOR
-            );
-            assert!(user_and_domain[0].len() > 6);
-        }
-
-        #[test]
-        fn both() {
-            let config = r#"
-                             prefix: 3
-                             suffix: 2
-                           "#;
-            let user_and_domain = user_and_domain(config);
-
-            assert_eq!(user_and_domain.len(), 2);
-            assert_eq!(user_and_domain[0].chars().nth(3).unwrap(), AFFIX_SEPARATOR);
-            assert_eq!(
-                user_and_domain[0].chars().nth_back(2).unwrap(),
-                AFFIX_SEPARATOR
-            );
-            assert!(user_and_domain[0].len() > 8);
-        }
+        assert_eq!(user_and_domain.len(), 2);
+        assert!(user_and_domain[1].starts_with("example."));
     }
 }
