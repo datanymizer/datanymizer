ffe759920018c1a4a122af0135a8016fd24b1678
Evgeniy Rashchepkin <e.a.rashchepkin@gmail.com>
2021-09-23 10:53:19 +0500

    Add QueryWrapper

--- datanymizer_dumper/src/postgres/dumper.rs
+++ datanymizer_dumper/src/postgres/dumper.rs
@@ -6,12 +6,56 @@ use crate::{Dumper, SchemaInspector, Table};
 use anyhow::Result;
 use datanymizer_engine::{Engine, Filter, Settings, TableList};
 use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
-use postgres::{Client, IsolationLevel, Transaction};
+use postgres::{
+    types::ToSql, Client, CopyOutReader, IsolationLevel, Row, ToStatement, Transaction,
+};
 use std::{
     io::{self, prelude::*},
     process::{self, Command},
     time::Instant,
 };
+
+enum QueryWrapper<'a> {
+    WithTransaction(Transaction<'a>),
+    WithoutTransaction(&'a mut Client),
+}
+
+impl<'a> QueryWrapper<'a> {
+    pub fn with_iso_level(cl: &'a mut Client, level: Option<IsolationLevel>) -> Result<Self> {
+        let wrapper = match level {
+            Some(level) => {
+                let tr = cl.build_transaction().isolation_level(level).start()?;
+                Self::WithTransaction(tr)
+            }
+            None => Self::WithoutTransaction(cl),
+        };
+        Ok(wrapper)
+    }
+
+    pub fn copy_out<T>(&mut self, query: &T) -> Result<CopyOutReader<'_>, postgres::Error>
+    where
+        T: ?Sized + ToStatement,
+    {
+        match self {
+            Self::WithTransaction(t) => t.copy_out(query),
+            Self::WithoutTransaction(c) => c.copy_out(query),
+        }
+    }
+
+    pub fn query_one<T>(
+        &mut self,
+        query: &T,
+        params: &[&(dyn ToSql + Sync)],
+    ) -> Result<Row, postgres::Error>
+    where
+        T: ?Sized + ToStatement,
+    {
+        match self {
+            Self::WithTransaction(t) => t.query_one(query, params),
+            Self::WithoutTransaction(c) => c.query_one(query, params),
+        }
+    }
+}
 
 pub struct PgDumper {
     schema_inspector: PgSchemaInspector,
